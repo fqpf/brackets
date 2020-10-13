f351f2eb53a7172433454dc4f45e7a9e4bfdc1e5
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-10-13 22:08:59 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function escapeRegExp(string){
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
+
+module.exports = function check(str, bracketsConfig) {
+    const regStr = bracketsConfig.reduce(
+        (res, configItem) => {
+            res.push(escapeRegExp(configItem.join('')));
+            return res;
+        },
+        []
+    ).join('|');
+    const reg = new RegExp(regStr, 'g');
+    let val = str;
+    while (reg.test(val)) {
+        val = val.replace(reg, '');
+    }
+    return !val.length
+};
